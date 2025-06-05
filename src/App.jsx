b0c4271850db6151e59a7366791acd815cb637b1
kira07sj <kirubeldagne1@gmail.com>
2025-06-05 15:58:45 +0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Router } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
